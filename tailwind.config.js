1327cd232a9d95c3994932c9a4ae1d30266d79a5
Sree Duggirala <sreevanthduggirala@gmail.com>
2023-03-04 11:32:49 -0600

    started front-end

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'imperial-red': '#E43E40',
+        'jet':'#2A2A2A',
+        'af-white':'#F5F5F7',
+      },
+    },
+  },
+  plugins: [],
+}
